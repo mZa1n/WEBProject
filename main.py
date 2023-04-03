026d6c59e33e1445ecc60be1a8db9a6457f78bec
Kirill <k9209107535@yandex.ru>
2023-04-03 21:43:42 +0300

    Add Reg and Login Forms

--- main.py
+++ main.py
@@ -1,15 +1,77 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response, session, redirect, jsonify, abort
+import datetime as dt
+from data import db_session
+from data.users import User
+from data.tasks import Tasks
+from forms.user import RegisterForm
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from forms.login import LoginForm
+from forms.task import TasksForm
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'timkarazvod'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('base.html')
 
 
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message='Пароли не совпадают')
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация', form=form,
+                                   message='Пользователь уже есть')
+        user = User(
+            login=form.login.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+    return render_template('reg.html', title='Регистрация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        else:
+            return render_template('log_in.html', message='Неправильный логин или пароль',
+                                   form=form)
+    return render_template('log_in.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
 def main():
+    db_session.global_init('db/users.db')
     app.run()
 
 
